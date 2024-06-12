9a5c11f903fe1aa72deda10836cd0287552abdcb
Sadhana <sadhanansingh.2023@gmail.com>
2024-06-12 21:02:45 +0530

    Practising functions

---
+++ custome_func.py
@@ -0,0 +1,11 @@
+def hello():
+    print("hello supi")
+    print("how are you!!")
+    print("you look fab today")
+
+name_1=input("ENter you name: ")
+
+if (name_1=="supi"):
+   hello()
+else:
+    print("you are not supi!!!! who you are??")
